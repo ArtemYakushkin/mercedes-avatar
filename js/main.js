742dc0143a9a178ae25a849a5b6838e85bae0e46
ArtemYakushkin <zimberger29101985@gmail.com>
2022-03-27 13:50:02 +0300

    сделал адаптив

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ $(function(){
         slidesToShow: 4,
         variableWidth: true,
         prevArrow: '<img class="arrow arrow-left" src="images/arrow-left.svg" alt="" />',
-        nextArrow: '<img class="arrow arrow-right" src="images/arrow-right.svg" alt="" />'
+        nextArrow: '<img class="arrow arrow-right" src="images/arrow-right.svg" alt="" />',
+        responsive: [
+            {
+                breakpoint: 361,
+                settings: {
+                    slidesToShow: 1,
+                    variableWidth: false,
+                }
+            }
+        ]
     });
 });
